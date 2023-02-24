aea6ed53d9d8548dba42d559f3bfd5891cfe9985
김규철 <rbcjf122@gmail.com>
2023-02-24 15:27:51 +0900

    🎉 :: Begin A Project

---
+++ WantedPreOnboarding/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WantedPreOnboarding
+//
+//  Created by 김규철 on 2023/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .systemPink
+    }
+
+
+}
+
