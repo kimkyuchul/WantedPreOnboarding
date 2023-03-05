d39f6e38258ed0d4647b1a54fd4c919250ad01f1
김규철 <rbcjf122@gmail.com>
2023-03-05 14:45:59 +0900

    👍 :: ImageView 구현

---
+++ WantedPreOnboarding/Feature/MainFeature/Sources/View/ImageView.swift
@@ -0,0 +1,54 @@
+//
+//  ImageView.swift
+//  WantedPreOnboarding
+//
+//  Created by 김규철 on 2023/03/05.
+//
+
+import UIKit
+
+final class ImageView: UIStackView {
+    
+    private let imageView = UIImageView(image: .init(systemName: "photo"))
+    
+    private let loadButton: UIButton = {
+        let button = UIButton(configuration: .filled())
+        button.setTitle("Load", for: .normal)
+        return button
+    }()
+    
+    private let progressView: UIProgressView = {
+        let progressView = UIProgressView(progressViewStyle: .bar)
+        progressView.setProgress(0.5, animated: true)
+        progressView.backgroundColor = .lightGray
+        progressView.layer.cornerRadius = 1
+        progressView.clipsToBounds = true
+        return progressView
+    }()
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        axis = .horizontal
+        alignment = .center
+        spacing = 10
+        
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setUp() {
+        
+        NSLayoutConstraint.activate([
+            imageView.widthAnchor.constraint(equalToConstant: 90),
+            imageView.heightAnchor.constraint(equalToConstant: 50)
+        ])
+        
+        [imageView, progressView, loadButton].forEach { subView in
+            subView.translatesAutoresizingMaskIntoConstraints = false
+            self.addArrangedSubview(subView)
+        }
+    }
+}
