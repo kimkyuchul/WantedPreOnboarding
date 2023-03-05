bd18016eb61b22d64a03c16f87d3f264b379bf64
김규철 <rbcjf122@gmail.com>
2023-03-05 17:07:12 +0900

    👍 :: ImageDownloadService 싱글턴 구현

---
+++ WantedPreOnboarding/Shared/ImageService.swift
@@ -0,0 +1,31 @@
+//
+//  ImageService.swift
+//  WantedPreOnboarding
+//
+//  Created by 김규철 on 2023/03/05.
+//
+
+import Foundation
+import UIKit
+
+public class ImageService {
+    public static let shared = ImageService()
+    
+    private init() { }
+    
+    func loadImage(from url: URL, success: @escaping((_ image:UIImage)->()), failure: @escaping ((_ msg:String)->())) {
+    
+        URLSession.shared.dataTask(with: url) { (data, urlrespinse, error) in
+            guard let data = data, error == nil else {
+                failure("Image cant download from G+ or fb server")
+                return
+            }
+            
+            DispatchQueue.main.async() {
+                if let img = UIImage(data: data) {
+                    success(img)
+                }
+            }
+        }.resume()
+    }
+}
